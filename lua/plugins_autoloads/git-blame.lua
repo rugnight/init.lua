c6cd9349dff1bfa61062c5d4ed68e5b914bf0274
rugnight <rugnight@gmail.com>
2025-03-31 21:11:35 +0900

    update

---
+++ lua/plugins_autoloads/git-blame.lua
@@ -0,0 +1,16 @@
+return {
+  'f-person/git-blame.nvim',
+  config = function()
+    vim.g.gitblame_enabled = 1
+    vim.g.gitblame_message_template = '<author> • <date> • <summary>'
+    vim.g.gitblame_date_format = '%Y/%m/%d %H:%M'
+    vim.g.gitblame_delay = 1000  -- ミリ秒
+    
+    -- バーチャルテキストの表示位置
+    vim.g.gitblame_message_when_not_committed = 'まだコミットされていません'
+    vim.g.gitblame_highlight_group = "LineNr"
+    
+    -- トグルするためのキーマッピング
+    vim.keymap.set('n', '<leader>gb', ':GitBlameToggle<CR>', { desc = 'GitBlame表示切替' })
+  end
+}
