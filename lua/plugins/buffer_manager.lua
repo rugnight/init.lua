73f2f07d456cb3b54646deeb4a8e069223052b43
rugnight <rugnight@gmail.com>
2025-03-31 21:17:22 +0900

    update

--- lua/plugins/buffer_manager.lua
+++ lua/plugins/buffer_manager.lua
@@ -5,7 +5,7 @@ return {
 	"j-morano/buffer_manager.nvim",
 	dependencies = "nvim-lua/plenary.nvim",
 	config = function() 
-		require("buffer_manager").setup()
+		require("lua.plugins.buffer_manager").setup()
 		vim.keymap.set("n", "<Leader>b", function() require("buffer_manager.ui").toggle_quick_menu() end)
 	end,
 }
