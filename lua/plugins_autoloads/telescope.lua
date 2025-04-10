7457c46d0d79920e4f3c4764142fac8547f63c8f
rugnight <rugnight@gmail.com>
2025-04-11 07:49:58 +0900

    update

--- lua/plugins_autoloads/telescope.lua
+++ lua/plugins_autoloads/telescope.lua
@@ -40,7 +40,7 @@ return {
             vim.keymap.set('n', '<leader>lw', builtin.lsp_dynamic_workspace_symbols, { desc = 'LSPワークスペースシンボル' })
             vim.keymap.set('n', '<leader>lr', builtin.lsp_references, { desc = 'LSP参照を検索' })
             vim.keymap.set('n', '<leader>ld', builtin.lsp_definitions, { desc = 'LSP定義を検索' })
-            vim.keymap.set('n', '<leader>li', builtin.lsp_implementations, { desc = 'LSP実装を検索' })
+            --vim.keymap.set('n', '<leader>li', builtin.lsp_implementations, { desc = 'LSP実装を検索' })
             vim.keymap.set('n', '<leader>lt', builtin.lsp_type_definitions, { desc = 'LSP型定義を検索' })
 
 			-- Git系
@@ -73,4 +73,33 @@ return {
 			--vim.keymap.set('n', '<leader>tr', ':Telescope frecency workspace=CWD path_display={"shorten"} theme=ivy<CR>', { desc = 'プロジェクト内で最近開いたファイル' })
 		end,
 	},
+	{
+		"jmacadie/telescope-hierarchy.nvim",
+		dependencies = {
+			{
+				"nvim-telescope/telescope.nvim",
+				dependencies = { "nvim-lua/plenary.nvim" },
+			},
+		},
+		keys = {
+			{ "<leader>li", "<cmd>Telescope hierarchy incoming_calls<cr>", desc = "LSP: 参照元を検索", },
+			{ "<leader>lo", "<cmd>Telescope hierarchy outgoing_calls<cr>", desc = "LSP: 参照先を検索", },
+		},
+		opts = {
+			-- don't use `defaults = { }` here, do this in the main telescope spec
+			extensions = {
+				hierarchy = {
+					-- telescope-hierarchy.nvim config, see below
+				},
+				-- no other extensions here, they can have their own spec too
+			},
+		},
+		config = function(_, opts)
+			-- Calling telescope's setup from multiple specs does not hurt, it will happily merge the
+			-- configs for us. We won't use data, as everything is in it's own namespace (telescope
+			-- defaults, as well as each extension).
+			require("telescope").setup(opts)
+			require("telescope").load_extension("hierarchy")
+		end,
+	}
 }
