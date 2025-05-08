d7f8cd245fbcbb31125f164c970c672336866b3c
rugnight <rugnight@gmail.com>
2025-05-08 21:59:15 +0900

    update

--- lua/plugins_autoloads/which-key.lua
+++ lua/plugins_autoloads/which-key.lua
@@ -9,6 +9,7 @@ return {
 		-- or leave it empty to use the default settings
 		-- refer to the configuration section below
 		triggers = {
+			{ "<auto>", mode = "nixsotc" },
 			{ "m", mode = { "n" } },
 		}
 	},
